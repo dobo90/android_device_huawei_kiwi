6dd3e2b2263071eeb26a16df3e85f651de9bad54
Dobroslaw Kijowski <dobo90@gmail.com>
2016-12-03 19:25:41 +0100

    * CM13ify device tree * Add prebuilt kernel and dt.img from cm-13.0-20161203-NIGHTLY-kiwi-recovery.img.
    Change-Id: Iaaf6cc70dd4105bf98a61f6371d11ef60bd23f12

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,25 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# config.mk
-#
-# Product-specific compile-time definitions.
-#
 
-LOCAL_PATH := device/huawei/kiwi
+DEVICE_PATH := device/huawei/kiwi
 
-# Assert
-# TARGET_OTA_ASSERT_DEVICE := 
-
-# Platform
-TARGET_BOARD_PLATFORM := msm8916
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno405
-
-# TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
-
-# Bootloader
-TARGET_NO_BOOTLOADER := true
-TARGET_BOOTLOADER_BOARD_NAME := msm8916
+TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
 # Architecture
 TARGET_ARCH := arm64
@@ -43,51 +28,63 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a7
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_BOARD_SUFFIX := _64
+TARGET_USES_64_BIT_BINDER := true
+TARGET_BOARD_PLATFORM := msm8916
 
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
-COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8916
+TARGET_NO_BOOTLOADER := true
+TARGET_OTA_ASSERT_DEVICE := kiwi
 
 # Kernel
-TARGET_CUSTOM_KERNEL_HEADERS := device/huawei/kiwi/include
-BOARD_KERNEL_BASE        := 0x80000000
-BOARD_KERNEL_PAGESIZE    := 2048
+BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/huawei/kiwi/recovery/dt.img
-TARGET_PREBUILT_KERNEL := device/huawei/kiwi/recovery/kernel
-#TARGET_USES_UNCOMPRESSED_KERNEL := true
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
+# Lines needed for prebuilt kernel:
+TARGET_KERNEL_SOURCE := # to make it work on pure cm13 tree
+TARGET_PREBUILT_KERNEL := device/huawei/kiwi/kernel
+BOARD_MKBOOTIMG_ARGS := --dt device/huawei/kiwi/dt.img
+# Uncomment lines below in order to compile kernel from source:
+# BOARD_KERNEL_SEPARATED_DT := true
+# BOARD_DTBTOOL_ARGS := -2
+# TARGET_KERNEL_SOURCE := kernel/huawei/kiwi
+# TARGET_KERNEL_ARCH := arm64
+# TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+# TARGET_KERNEL_HEADER_ARCH := arm64
+# TARGET_USES_UNCOMPRESSED_KERNEL := true
+# TARGET_KERNEL_CONFIG := cyanogenmod_kiwi-64_defconfig
+
+# Init
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
 # Partitions
-BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
 # /proc/partitions * 2 * BLOCK_SIZE (512) = size in bytes
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 11618204672
-# blockdev --getbsz /dev/block/mmcblk0p19
-BOARD_FLASH_BLOCK_SIZE := 4096
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 67108864
+BOARD_FLASH_BLOCK_SIZE := 131072 # blockdev --getbsz /dev/block/mmcblk0p19
+
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
 # TWRP
 RECOVERY_VARIANT := twrp
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/etc/twrp.fstab
 TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 BOARD_SUPPRESS_SECURE_ERASE := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TWHAVE_SELINUX := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 TW_INPUT_BLACKLIST := "accelerometer"
 TW_NO_EXFAT_FUSE := true
-
-# Vold
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-# BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
-BOARD_VOLD_MAX_PARTITIONS := 65
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+TW_INCLUDE_CRYPTO := true
--- full_kiwi.mk
+++ full_kiwi.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,21 +12,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-LOCAL_PATH := device/huawei/kiwi
-
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Screen density
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+# Inherit from kiwi device
+$(call inherit-product, device/huawei/kiwi/device.mk)
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-
-PRODUCT_NAME := full_kiwi
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kiwi
+PRODUCT_NAME := full_kiwi
+PRODUCT_BRAND := HONOR
+PRODUCT_MODEL := KIW-L24
 PRODUCT_MANUFACTURER := HUAWEI
-PRODUCT_MODEL := kiwi
